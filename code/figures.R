63c3ecd5a09fdf4163a92acd9af03b3fbaa4aaf8
robwschlegel <robwschlegel@gmail.com>
2020-08-24 13:46:29 -0300

    Created new RMSE results figure and changed the figure numbers for the existing figs

--- code/figures.R
+++ code/figures.R
@@ -7,6 +7,33 @@
 
 source("code/functions.R")
 
+# The correlations
+ALL_cor_fig <- readRDS("data/ALL_cor.Rda") %>% 
+  mutate(region = toupper(region)) %>% 
+  mutate(region = factor(region, levels = c("MAB", "GM", "SS", "CBS", "GSL", "NFS")),
+         season = factor(season, levels = c("Spring", "Summer", "Autumn", "Winter")),
+         Parameter2 = as.character(Parameter2)) %>% 
+  filter(Parameter1 == "sst") %>% #,
+  # ts != "full") %>% 
+  mutate(Parameter2 = case_when(Parameter2 == "sst" ~ "SST",
+                                Parameter2 == "bottomT" ~ "Bottom",
+                                Parameter2 == "sss" ~ "SSS",
+                                Parameter2 == "mld_cum" ~ "MLD",
+                                Parameter2 == "mld_1_cum" ~ "MLD_1",
+                                Parameter2 == "t2m" ~ "Air",
+                                Parameter2 == "tcc_cum" ~ "Cloud",
+                                Parameter2 == "p_e_cum" ~ "P-E",
+                                Parameter2 == "mslp_cum" ~ "MSLP",
+                                Parameter2 == "lwr_budget" ~ "Qlw",
+                                Parameter2 == "swr_budget" ~ "Qsw",
+                                Parameter2 == "lhf_budget" ~ "Qlh",
+                                Parameter2 == "shf_budget" ~ "Qsh",
+                                Parameter2 == "qnet_budget" ~ "Qnet",
+                                TRUE ~ Parameter2),
+         Parameter2 = factor(Parameter2, levels = c("Qnet", "Qlh", "Qsh", "Qlw", "Qsw",
+                                                    "Air", "Cloud", "P-E", "MSLP",
+                                                    "SST", "SSS", "MLD", "MLD_1", "Bottom")))
+
 
 # Figure 1 ----------------------------------------------------------------
 
@@ -95,38 +122,45 @@ ggsave("figures/fig_1.png", fig_1, height = 5, width = 10)
 
 # Figure 2 ----------------------------------------------------------------
 
-# Histogram of r values
+# Boxplots showing range of RMSE results per region and season
 
-# The correlations
-ALL_cor <- readRDS("data/ALL_cor.Rda") %>% 
-  mutate(region = toupper(region)) %>% 
-  mutate(region = factor(region, levels = c("MAB", "GM", "SS", "CBS", "GSL", "NFS")),
-         season = factor(season, levels = c("Spring", "Summer", "Autumn", "Winter")),
-         Parameter2 = as.character(Parameter2)) %>% 
-  filter(Parameter1 == "sst",
-         ts != "full") %>% 
-  mutate(Parameter2 = case_when(Parameter2 == "sst" ~ "SST",
-                                Parameter2 == "bottomT" ~ "Bottom",
-                                Parameter2 == "sss" ~ "SSS",
-                                Parameter2 == "mld_cum" ~ "MLD",
-                                Parameter2 == "mld_1_cum" ~ "MLD_1",
-                                Parameter2 == "t2m" ~ "Air",
-                                Parameter2 == "tcc_cum" ~ "Cloud",
-                                Parameter2 == "p_e_cum" ~ "P-E",
-                                Parameter2 == "mslp_cum" ~ "MSLP",
-                                Parameter2 == "lwr_budget" ~ "Qlw",
-                                Parameter2 == "swr_budget" ~ "Qsw",
-                                Parameter2 == "lhf_budget" ~ "Qlh",
-                                Parameter2 == "shf_budget" ~ "Qsh",
-                                Parameter2 == "qnet_budget" ~ "Qnet",
-                                TRUE ~ Parameter2),
-         Parameter2 = factor(Parameter2, levels = c("Qnet", "Qlh", "Qsh", "Qlw", "Qsw",
-                                                    "Air", "Cloud", "P-E", "MSLP",
-                                                    "SST", "SSS", "MLD", "MLD_1", "Bottom")))
+# Prep data
+ALL_RMSE_fig <- ALL_cor_fig %>% 
+  filter(rmse > 0 ) %>% 
+  dplyr::select(region, season, ts, Parameter2, rmse)
+
+# Region boxplot
+fig_2a <- ALL_RMSE_fig %>% 
+  ggplot(aes(x = Parameter2, y = rmse)) +
+  geom_boxplot(aes(fill = ts)) +
+  scale_fill_brewer(palette = "Set2") +
+  facet_wrap(~region) +
+  labs(x = NULL, y = "RMSE", fill = "Time series")
+# fig_2a
+
+# Season boxplot
+fig_2b <- ALL_RMSE_fig %>% 
+  ggplot(aes(x = Parameter2, y = rmse)) +
+  geom_boxplot(aes(fill = ts)) +
+  scale_fill_brewer(palette = "Set2") +
+  facet_wrap(~season) +
+  labs(x = NULL, y = "RMSE", fill = "Time series")
+# fig_2b
+
+# Combine and save
+fig_2 <- ggpubr::ggarrange(fig_2a, fig_2b, ncol = 2, nrow = 1, labels = c("A)", "B)"), 
+                          widths = c(3, 2), align = "h", common.legend = T)
+# fig_2
+ggsave("figures/fig_2.png", fig_2, height = 5, width = 10)
+
+
+# Figure 3 ----------------------------------------------------------------
+
+# Histogram of r values
 
 # Function for plotting histograms of chosen variables
 hist_var <- function(var_choices, y_label){
-  ALL_cor %>% 
+  ALL_cor_fig %>% 
     filter(Parameter2 %in% var_choices) %>% 
     ggplot(aes(x = r)) +
     geom_vline(aes(xintercept = 0), colour = "red", size = 1) +
@@ -149,27 +183,28 @@ hist_ocean <- hist_var(c("SSS", "MLD", "Bottom"), "Ocean")
 hist_bottom <- ggpubr::ggarrange(hist_air, hist_ocean, ncol = 2, nrow = 1, align = "h",  labels = c("B)", "C)"))
 
 # The final figure
-fig_2 <- ggpubr::ggarrange(hist_Q, hist_bottom, ncol = 1, nrow = 2, align = "h",  labels = c("A)"))
-# fig_2
-ggsave("figures/fig_2.png", fig_2, height = 5, width = 10)
+fig_3 <- ggpubr::ggarrange(hist_Q, hist_bottom, ncol = 1, nrow = 2, align = "h",  labels = c("A)"))
+# fig_3
+ggsave("figures/fig_3.png", fig_3, height = 7, width = 10)
 
 
-# Figure 3 ----------------------------------------------------------------
+# Figure 4 ----------------------------------------------------------------
 
 # Most important variables by region/season
 boxplot_var <-  function(var_choices, y_label){
-  ALL_cor %>% 
+  ALL_cor_fig %>% 
     filter(Parameter2 %in% var_choices) %>% 
     ggplot(aes(x = ts, y = r)) +
     geom_hline(aes(yintercept = 0), colour = "red", size = 1) +
     geom_boxplot(aes(fill = season), notch = F) +
+    # geom_violin(aes(fill = season)) + # Looks bad
     facet_wrap(~Parameter2, nrow = 1) +
     scale_fill_manual(values = c("#a99a35", "#8baa43", "#e89c3c", "#9a9997")) + # muted
     # scale_fill_manual(values = c("#d6cf36", "#a5bfe4", "#efbe83", "#b6b6b4")) + # larger spread
     # scale_y_continuous(expand = c(0, 0)) +
     # scale_x_continuous(expand = c(0, 0)) +
     # facet_grid(ts ~ Parameter2) +
-    coord_equal() +
+    coord_equal(ratio = 1.5) +
     labs(x = NULL, y = y_label, fill = "Region") +
     theme(axis.title.y = element_text(size = 16),
           panel.background = element_rect(colour = "black"))
@@ -183,24 +218,24 @@ box_ocean <- boxplot_var(c("SSS", "MLD", "Bottom"), "Ocean")
 box_bottom <- ggpubr::ggarrange(box_air, box_ocean, ncol = 2, nrow = 1, align = "h",  labels = c("B)", "C)"), legend = "none")
 
 # The final figure
-fig_3 <- ggpubr::ggarrange(box_Q, box_bottom, ncol = 1, nrow = 2, align = "h",  labels = c("A)"), common.legend = T)
-# fig_3
-ggsave("figures/fig_3.png", fig_3, height = 5, width = 10)
+fig_4 <- ggpubr::ggarrange(box_Q, box_bottom, ncol = 1, nrow = 2, align = "h",  labels = c("A)"), common.legend = T)
+# fig_4
+ggsave("figures/fig_4.png", fig_4, height = 5, width = 10)
 
 
-# Figure 4 ----------------------------------------------------------------
+# Figure 5 ----------------------------------------------------------------
 
 # SOM region + season panels.
 # Created in the MHWNWA project.
 
 
-# Figure 5 ----------------------------------------------------------------
+# Figure 6 ----------------------------------------------------------------
 
 # SOM atmosphere panels.
 # Created in the MHWNWA project.
 
 
-# Figure 6 ----------------------------------------------------------------
+# Figure 7 ----------------------------------------------------------------
 
 # The correlation results clustered by SOM node
 
@@ -218,7 +253,7 @@ events_cor_prep <- OISST_MHW_event %>%
   left_join(ALL_cor_wide, by = c("region", "season", "event_no")) %>% 
   # ungroup() %>% 
   dplyr::select(region:n_Obs, sst, bottomT, sss, mld_cum, mld_1_cum, t2m, tcc_cum, p_e_cum, mslp_cum,
-                lwr_mld_cum, swr_mld_cum, lhf_mld_cum, shf_mld_cum, qnet_mld_cum)
+                lwr_budget, swr_budget, lhf_budget, shf_budget, qnet_budget)
 
 
 # Load the SOM from the MHWNWA
@@ -230,11 +265,11 @@ SOM_info <- SOM$info
 # Join to the GLORYS MHW correlation results
 events_cor_SOM <- left_join(events_cor_prep, SOM_info, by = c("region", "event_no"))
 
-fig_6 <- events_cor_SOM %>% 
-  dplyr::select(node, ts, bottomT:qnet_mld_cum, -mld_1_cum) %>% 
+fig_7 <- events_cor_SOM %>% 
+  dplyr::select(node, ts, bottomT:qnet_budget, -mld_1_cum) %>% 
   group_by(node, ts) %>% 
   summarise_if(is.numeric, mean) %>% 
-  pivot_longer(cols = bottomT:qnet_mld_cum) %>%
+  pivot_longer(cols = bottomT:qnet_budget) %>%
   filter(name != "temp",
          ts != "full") %>%
   ungroup() %>% 
@@ -249,11 +284,11 @@ fig_6 <- events_cor_SOM %>%
                           name == "tcc_cum" ~ "Cloud",
                           name == "p_e_cum" ~ "P_E",
                           name == "mslp_cum" ~ "MSLP",
-                          name == "lwr_mld_cum" ~ "Qlw",
-                          name == "swr_mld_cum" ~ "Qsw",
-                          name == "lhf_mld_cum" ~ "Qlh",
-                          name == "shf_mld_cum" ~ "Qsh",
-                          name == "qnet_mld_cum" ~ "Qnet",
+                          name == "lwr_budget" ~ "Qlw",
+                          name == "swr_budget" ~ "Qsw",
+                          name == "lhf_budget" ~ "Qlh",
+                          name == "shf_budget" ~ "Qsh",
+                          name == "qnet_budget" ~ "Qnet",
                           TRUE ~ name),
          name = factor(name, levels = c("Qnet", "Qlw", "Qsw", "Qlh", "Qsh", "Cloud", 
                                         "P_E", "Air", "MSLP", "MLD", "SSS", "Bottom"))) %>% 
@@ -267,6 +302,6 @@ fig_6 <- events_cor_SOM %>%
         axis.text.y = element_text(angle = 90, hjust = 0.5),
         axis.text.x = element_text(angle = 30, hjust = 1.0),
         panel.background = element_rect(colour = "black"))
-# fig_6
-ggsave("figures/fig_6.png", fig_6, height = 7, width = 14)
+# fig_7
+ggsave("figures/fig_7.png", fig_7, height = 7, width = 14)
 
